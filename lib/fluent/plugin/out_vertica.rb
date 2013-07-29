ffbc470325af29c564b37b3e3f67bcf300269a2c
Erik Selin <erik.selin@jadedpixel.com>
2013-07-29 11:33:35 -0400

    updated plugin to work in a more production ready manner.

--- lib/fluent/plugin/out_vertica.rb
+++ lib/fluent/plugin/out_vertica.rb
@@ -10,7 +10,6 @@ module Fluent
     config_param :schema,         :string,  :default => nil
     config_param :table,          :string,  :default => nil
     config_param :ssl,            :bool,    :default => false
-    config_param :enforce_length, :bool,    :default => false
 
     def initialize
       super
@@ -26,23 +25,77 @@ module Fluent
 
     def write(chunk)
       chunk.open do |file|
-        copy_sql = <<-SQL
-            COPY #{@schema}.#{@table} (#{columns.join(",")})
-            FROM STDIN DELIMITER E'\t'
-          RECORD TERMINATOR E'\n' NULL AS '__NULL__'
-          ENFORCELENGTH
-           ABORT ON ERROR
-         TRICKLE
-         #{"ENFORCELENGTH" if @enforce_length}
+
+        reset
+
+        temp_table = "temp_#{@table}"
+        perm_table = "#{@schema}.#{@table}"
+
+        vertica.query(<<-SQL)
+          CREATE LOCAL TEMPORARY TABLE #{temp_table}
+          ON COMMIT DELETE ROWS
+          AS SELECT * FROM #{perm_table} LIMIT 0
         SQL
 
-        vertica.copy(copy_sql) do |copy_handle|
-          copy_handle.write(file.read)
+        vertica.copy(<<-SQL) { |handle| handle.write(file.read) }
+          COPY #{temp_table} (#{columns.join(",")})
+          FROM STDIN DELIMITER E'\t'
+          RECORD TERMINATOR E'\n' NULL AS '__NULL__'
+          ENFORCELENGTH
+          NO COMMIT
+        SQL
+
+        if primary_keys.empty?
+          vertica.query(<<-SQL)
+            INSERT INTO #{perm_table}
+              (#{columns.join(",")})
+            SELECT
+              #{columns.join(",")}
+            FROM #{temp_table}
+          SQL
+        else
+          condition = primary_keys.map do |key|
+            "#{perm_table}.#{key} = #{temp_table}.#{key}"
+          end
+
+          unless empty_table?(perm_table)
+            vertica.query(<<-SQL)
+              DELETE FROM #{perm_table}
+              WHERE EXISTS (
+                SELECT 1
+                FROM #{temp_table}
+                WHERE #{condition.join(" AND ")}
+              )
+            SQL
+          end
+
+          vertica.query(<<-SQL)
+            INSERT INTO #{perm_table}
+              (#{columns.join(",")})
+            SELECT
+              #{columns.join(",")}
+            FROM (
+              SELECT
+                #{columns.join(",")},
+                row_number() OVER (partition by #{primary_keys.join(",")}) AS r
+              FROM #{temp_table}
+            ) AS temp
+            WHERE r = 1
+          SQL
         end
+
+        vertica.query("COMMIT")
       end
     end
 
     private
+
+    def reset
+      @vertica      = nil
+      @columns      = nil
+      @primary_keys = nil
+      @empty        = nil
+    end
 
     def vertica
       @vertica ||= Vertica.connect({
@@ -61,8 +114,23 @@ module Fluent
              FROM columns
             WHERE table_schema ='#{@schema}'
               AND table_name='#{@table}'
-         ORDER BY ordinal_position;
+         ORDER BY ordinal_position
        SQL
+    end
+
+    def primary_keys
+      @primary_keys ||= vertica.query(<<-SQL).map { |column| column[:column_name] }
+        SELECT column_name
+          FROM v_catalog.primary_keys
+         WHERE table_schema = '#{@schema}'
+           AND table_name = '#{@table}'
+      SQL
+    end
+
+    def empty_table?(table)
+      @empty ||= vertica.query(<<-SQL).first[:count] == 0
+        SELECT count(1) FROM #{table};
+      SQL
     end
   end
 end
